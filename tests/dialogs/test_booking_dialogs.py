9dcd8a373fa72ef0d6e3a4780d468681630eefb2
Sébastien MOUGEL <smougel@yahoo.fr>
2021-01-28 16:33:02 +0100

    Add tests for BookingDialog class

--- tests/dialogs/test_booking_dialogs.py
+++ tests/dialogs/test_booking_dialogs.py
@@ -7,3 +7,21 @@ class TestBookingDialog():
         bd.set_logger("a")
 
         assert bd.logger == "a"
+
+    def test_is_ambiguous(self):
+
+        bd = bd = BookingDialog()
+        ret = bd.is_ambiguous("what")
+        
+        assert ret
+
+        ret = bd.is_ambiguous("2020-01-01")
+        
+        assert not ret
+
+    def test_set_metrics_exporter(self):
+        
+        bd = bd = BookingDialog()
+        bd.set_metrics_exporter("something")
+        
+        assert bd.metrics_exporter == "something"
